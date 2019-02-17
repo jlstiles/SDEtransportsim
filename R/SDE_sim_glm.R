6e564516f022f6ff6e25867d92d18b4b5ab4d011
Jonathan Levy <jl@jlstiles.com>
2019-02-17 12:38:07 -0800

    still, yes, still a fuck up in there

--- R/SDE_sim_glm.R
+++ R/SDE_sim_glm.R
@@ -329,7 +329,7 @@ get_gstarM_glm  = function(data, truth, forms)
     }
     
     get_cc_eff0 = function(data, gstarM_astar, a) {
-      with(data, ((S == 1)*(A == a)*
+      with(data, ((S == 1)*
                     ((M == 1)*gstarM_astar + (M == 0)*(1 - gstarM_astar))*
                     ((Z == 1)*ZS0_ps0 + (Z == 0)*(1 - ZS0_ps0))*(1 - S_ps0))/
              (((M == 1)*M_ps0 + (M == 0)*(1 - M_ps0))*
